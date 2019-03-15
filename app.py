d59ac032858725ff453916d2c2241a5360872e98
wangze <1401698637@qq.com>
2019-03-15 15:38:27 +0800

    use template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import make_response
 from flask import request
 from flask import redirect
@@ -17,12 +17,12 @@ def index():
     # response = make_response('<h1> This document carries a cookie!</h1>')
     # response.set_cookie('answer','42')
     # return response
-    return redirect('http://www.example.com')
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!<h1>' % name
+    return render_template('user.html', name=name)
 
 
 # @app.route('/user/<id>')
